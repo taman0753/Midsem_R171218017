80ff3046039d7eddd1631e6a7f2f9f9fa1924bdb
taman0753 <taman0753@gmail.com>
2019-03-18 10:30:28 +0530

    second commit

--- 1.cpp
+++ 1.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main()
-{ int rollno;
+{ int rollno,marks;
 	cout<<"enter your roll number";
 	cin>>rollno;
-	cout<<"your roll no is"<<rollno;
+	cout<<"your roll no is"<<rollno;//first increament in the code
+ cout<<"enter your marks";
+        cin>>marks;
+        cout<<"your marks are"<<marks;//second increment in the code
 
 
 
