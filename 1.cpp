c779906dd03fe9cf8abf30b0cb9ebd1d65b384da
taman0753 <taman0753@gmail.com>
2019-03-18 10:32:56 +0530

    third commit

--- 1.cpp
+++ 1.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 int main()
-{ int rollno,marks;
+{ int rollno,marks,percentage;
 	cout<<"enter your roll number";
 	cin>>rollno;
 	cout<<"your roll no is"<<rollno;//first increament in the code
  cout<<"enter your marks";
         cin>>marks;
         cout<<"your marks are"<<marks;//second increment in the code
+	 cout<<"enter your percentage";
+        cin>>percentage;
+        cout<<"your percentage is"<<percentage;//third increament in the code
 
 
 
